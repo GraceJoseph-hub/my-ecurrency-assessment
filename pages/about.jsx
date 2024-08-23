9170ae61ae27557d4bbac525cc6424e28cebb42f
Grace <gracecossai@gmail.com>
2024-08-23 18:31:00 +0100

    Added an about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,20 @@
+import Hero from '@/components/homeComponents/Hero'
 import Layout from '@/components/Layout'
-import React from 'react'
+import mainBg from '../public/images/mainBg.png'
+import BannerText from '@/components/globalComponents/BannerText'
 
 const about = () => {
   return (
-    <Layout>This is the about page</Layout>
-  )
+    <Layout>
+      <Hero backgroundImage={mainBg} altText="Hero banner" showImage={false}>
+        <BannerText
+          isParaTwo={true}
+          paraTwo="With the aid of our Melatonin Sleepstiq, we can assure you that you can get quality sleep."
+          isBtn={true}
+        />
+      </Hero>
+    </Layout>
+  );
 }
 
 export default about
