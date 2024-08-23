6dd304ff4b2d5162c256496f2503aaa75161c869
Grace <gracecossai@gmail.com>
2024-08-23 18:16:38 +0100

    Escaped a character

--- components/globalComponents/BannerText.jsx
+++ components/globalComponents/BannerText.jsx
@@ -3,7 +3,7 @@ import Button from './Button';
 const BannerText = ({ paraTwo, isParaTwo, isBtn}) => {
   return (
     <div className="font-Poppins">
-      <p className="text-[#12305B]">We're here to help you</p>
+      <p className="text-[#12305B]">We&apos;re here to help you</p>
       <h1 className="text-[60px] font-bold">Relax & Rest</h1>
       {isParaTwo && <p className="mb-4">{paraTwo}</p>}
       {isBtn && <Button text="View Shop" />}
